423f1db7e6885d36cbe76a0c23b8aebef4d90596
FormosoCrl <davidformosocorral@gmail.com>
2026-05-05 11:27:31 +0200

    Activity de Login marcada como launcher y navegación entre login, registro y lobby

--- PixelDungeons-FrontEnd/app/src/main/java/com/example/pixeldungeons/ui/LobbyActivity.java
+++ PixelDungeons-FrontEnd/app/src/main/java/com/example/pixeldungeons/ui/LobbyActivity.java
@@ -1,6 +1,8 @@
 package com.example.pixeldungeons.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +23,19 @@ public class LobbyActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button searchButton = findViewById(R.id.search_button);
+        Button createButton = findViewById(R.id.create_button);
+
+        searchButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LobbyActivity.this, SearchRoomActivity.class);
+            startActivity(intent);
+        });
+
+        createButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LobbyActivity.this, CreateRoomActivity.class);
+            startActivity(intent);
         });
     }
 }

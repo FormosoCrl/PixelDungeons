0a272261ac9311903e2424dffffe46acf636748f
FormosoCrl <davidformosocorral@gmail.com>
2026-05-05 16:04:00 +0200

    Navegación rama del jugador y adapter de personajes

--- PixelDungeons-FrontEnd/app/src/main/java/com/example/pixeldungeons/ui/SearchRoomActivity.java
+++ PixelDungeons-FrontEnd/app/src/main/java/com/example/pixeldungeons/ui/SearchRoomActivity.java
@@ -1,6 +1,9 @@
 package com.example.pixeldungeons.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,15 @@ public class SearchRoomActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText nameInput = findViewById(R.id.name_input);
+        Button searchButton = findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(v -> {
+            Intent intent = new Intent(SearchRoomActivity.this, CharacterListActivity.class);
+            intent.putExtra("room_name", nameInput.getText().toString());
+            startActivity(intent);
         });
     }
 }
